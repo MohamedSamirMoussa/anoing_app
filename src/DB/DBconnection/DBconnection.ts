a80090bb3d832a5fdd6216b2dc7aa983383be447
MohamedSamirMoussa <mosamir5000@gmail.com>
2026-01-30 05:31:35 +0200

    fix

--- src/DB/DBconnection/DBconnection.ts
+++ src/DB/DBconnection/DBconnection.ts
@@ -5,9 +5,10 @@ export const DBconnection = async () => {
     if (!process.env.DB_HOST) throw new Error("DB_HOST is not defined");
 
     await connect(process.env.DB_HOST as string, {
-      serverSelectionTimeoutMS: 10000,
+      serverSelectionTimeoutMS: 30000,
       family: 4,
-      socketTimeoutMS: 4500,
+      socketTimeoutMS: 30000,
+      connectTimeoutMS: 30000,
     });
     console.log("DB connected successfully");
   } catch (error) {

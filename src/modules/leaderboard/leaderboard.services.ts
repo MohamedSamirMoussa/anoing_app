40c4199310bcc23b60e8427c2af4722c7d4946b7
MohamedSamirMoussa <mosamir5000@gmail.com>
2026-01-31 16:41:21 +0200

    fix

--- src/modules/leaderboard/leaderboard.services.ts
+++ src/modules/leaderboard/leaderboard.services.ts
@@ -1,9 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import {
-  ILeaderboardUser,
-  LeaderboardModel,
-  LeaderboardRepository,
-} from "../../DB";
+import { LeaderboardModel, LeaderboardRepository } from "../../DB";
 import {
   BadRequestError,
   getConnectionWithServer,
@@ -23,7 +19,7 @@ class LeaderboardServices {
   ) {
     if (this.isAutoUpdating) return;
     this.isAutoUpdating = true;
-    const servers = ["AllTheMons"];
+    const servers = ["AllTheMons" , "atm 10"];
     const update = async () => {
       try {
         for (const serverName of servers) {
@@ -67,53 +63,30 @@ class LeaderboardServices {
     next: NextFunction,
   ): Promise<Response | void> => {
     try {
-      const serverName = (req.query.serverName as string) || "atm 10";
-      // const page = parseInt(req.query.page as string);
-      // const limit = parseInt(req.query.limit as string);
-      // const skip = (page - 1) * limit;
+      const { serverName } = req.query;
+      let users ;
 
-      let sourceData: ILeaderboardUser[] = await this.leaderboardModel.find({
-        filter: { serverName },
-        options: { sort: { "playTime.hours": -1 } },
-      });
+      const { sortedLeaderboard, onlineCount } = await getConnectionWithServer(
+        serverName as string,
+      );
 
-      let onlineCount = 0;
-
-      if (sourceData.length === 0) {
-        console.log("DB is empty, fetching from Server...");
-        const response = await getConnectionWithServer(serverName);
-        sourceData = response?.sortedLeaderboard || [];
-        onlineCount = response?.onlineCount || 0;
-      } else {
-        try {
-          const fastCheck = (await Promise.race([
-            getConnectionWithServer(serverName),
-            new Promise((_, reject) => setTimeout(() => reject(), 1500)), // 1.5 ثانية بالظبط
-          ])) as any;
-          onlineCount = fastCheck?.onlineCount || 0;
-
-          const onlineUsers = fastCheck?.sortedLeaderboard
-            ?.filter((u: any) => u.is_online)
-            .map((u: any) => u.username);
-          sourceData = sourceData.map((user) => ({
-            ...user,
-            is_online: onlineUsers?.includes(user.username) || false,
-          }));
-        } catch (e) {}
+      if (!sortedLeaderboard || sortedLeaderboard.length === 0) {
+         users = await this.leaderboardModel.find({
+          filter:{serverName}
+        })
       }
 
-      const paginatedData = sourceData.slice(skip, skip + limit);
 
       return successHandler({
         res,
         result: {
-          leaderboard: paginatedData,
+          leaderboard: sortedLeaderboard || users,
           onlineCount,
           pagination: {
-            // currentPage: page,
-            // totalPages: Math.ceil(sourceData.length / limit) || 0,
-            totalItems: sourceData.length,
-            // limit,
+            currentPage: page,
+            totalPages: Math.ceil(sortedLeaderboard.length / limit),
+            totalItems: sortedLeaderboard.length,
+            limit,
           },
         },
       });
@@ -121,6 +94,7 @@ class LeaderboardServices {
       next(error);
     }
   };
+
   searchPlayers = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const { username } = req.query;
@@ -131,7 +105,7 @@ class LeaderboardServices {
 
       const searchResult = await this.leaderboardModel.find({
         filter: { username: { $regex: username, $options: "i" } },
-      });
+      })
 
       return successHandler({
         res,
